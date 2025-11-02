021c146f344a8ae1d71df52233343fe639b8faaf
DuncanH26 <duncan.hemelstrand@g.austincc.edu>
2025-11-02 04:55:52 +0000

    Homework 6

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,74 @@
 //cpp
+#include "tic_tac_toe.h"
+
+using strd::cout;
+
+bool TicTacToe::game_over()
+{
+   
+    return check_board_full();
+
+}
+
+void TicTacToe::start_game(std::string first_player){
+
+    player = first_player;
+    clear_board();
+}
+
+ void mark_board(int position){
+
+    pegs[position - 1] = player;
+    set_next_player();
+ }
+
+
+ void TicTacToe::display_board() const{
+    for(int i=0; i < peg.size(); i+=3){
+
+        cout<<pegs[i]<<"|"<<pegs[i+1]<<"|"<<pegs[i+2]<<"\n";
+
+
+    }
+
+
+ }
+
+void set_next_player()
+{
+
+    if(player == 'X'){
+        player = "0";
+    }
+    else{
+
+        player = "X";
+    }
+
+}
+
+bool TicTacToe::check_board_full()
+{
+
+    auto full = true;
+
+    for(auto &peg: pegs)
+    {
+        if(peg == " "){
+
+            full = false;
+            break;
+        }
+
+    }
+    return full;
+}
+
+void TicTacToe::clear_board()
+{
+    for(auto &peg: pegs)
+    {
+        peg = " ";
+    }
+
+}
